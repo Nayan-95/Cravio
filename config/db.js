49ad22775814db95f8a4f98bea407749d215a4bf
Nayan-95 <112968914+Nayan-95@users.noreply.github.com>
2025-06-03 16:03:29 +0530

    changed dp link
    changed dp link

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   try {
     const username = process.env.DB_USERNAME;
     const password = process.env.DB_PASSWORD;
-    const URL = `mongodb://${username}:${password}@ac-0irl3ft-shard-00-00.fxahxyi.mongodb.net:27017,ac-0irl3ft-shard-00-01.fxahxyi.mongodb.net:27017,ac-0irl3ft-shard-00-02.fxahxyi.mongodb.net:27017/stall-savvy?ssl=true&replicaSet=atlas-idc13v-shard-0&authSource=admin&retryWrites=true&w=majority&appName=blog-app`;
+    const URL = `mongodb://${username}:${password}@ac-0irl3ft-shard-00-00.fxahxyi.mongodb.net:27017,ac-0irl3ft-shard-00-01.fxahxyi.mongodb.net:27017,ac-0irl3ft-shard-00-02.fxahxyi.mongodb.net:27017/cravio?ssl=true&replicaSet=atlas-idc13v-shard-0&authSource=admin&retryWrites=true&w=majority&appName=blog-app`;
     await mongoose.connect(URL);
     console.log('MongoDB connected');
   } catch (err) {
